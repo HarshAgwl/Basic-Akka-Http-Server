173b74d565256ebc3586067841714be3d9a1e2ba
Harsh Agrawal <harsh51000@gmail.com>
2020-04-27 17:57:23 +0530

    Deleted create-and-update-deployment.sh

--- create-and-update-deployment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $GO_DEPENDENCY_LABEL_PIPELINE_MATERIAL -gt 1 ]
-then
-  kubectl set image deployment/example-akka-http-server basic-akka-http-app=basic-akka-http-app:$GO_DEPENDENCY_LABEL_PIPELINE_MATERIAL
-else
-	kubectl create deployment example-akka-http-server --image=itsharshag/basic-akka-http-app:4
-	kubectl expose deployment example-akka-http-server --type=NodePort --port=8080
-fi
